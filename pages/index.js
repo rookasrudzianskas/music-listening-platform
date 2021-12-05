0b17d9fdca2de62176e8ce75e415f978005d6a4b
Rokas <rookas.rudzenskas@gmail.com>
2021-12-05 10:19:44 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Spotify ✳️</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Done</h1>
