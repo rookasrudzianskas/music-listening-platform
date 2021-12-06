928ae890dfeae0fa8c6948df4cceef7f2ca3ff8a
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:03:42 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Sidebar from "../components/Sidebar";
 
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       </Head>
 
         <main>
-            {/*    sidebar  */}
+            <Sidebar />
             {/*    center   */}
         </main>
 
