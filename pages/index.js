d466af9f0ccbc590fa8ea0b8c81e060a8d8332c8
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 13:12:25 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,12 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Spotify ✳️</title>
       </Head>
 
-        <div className="hover:cursor-pointer">
-            <h1 className="text-6xl font-bold text-black ">Spotify Clone 🔥</h1>
-        </div>
+        div
 
     </div>
   )
